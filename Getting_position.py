d92f49dfe4b45fc673c84cd277a44b1e5f7d093d
Tenshipy <71231507+Tenshipy@users.noreply.github.com>
2020-09-14 16:16:44 +0200

    Update Getting_position.py

--- Getting_position.py
+++ Getting_position.py
@@ -4,3 +4,6 @@ import pyautogui
 coords = {'start_solidx': 695, 'start_solidy' : 1063,
           'Start_filex': 218, 'Start_filey': 16,
           'openx': 401, 'openy': 372}
+
+
+# You can use like num3 to get a possition of your cursos
